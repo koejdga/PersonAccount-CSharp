3f9390ac9b113e414d9be61a7613d0da4888b3f1
koejdga <avolidub@gmail.com>
2023-03-22 09:37:52 +0200

    Add project

---
+++ Person/Exceptions/WrongBirthDateException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Exceptions
+{
+    public class WrongBirthDateException : Exception
+    {
+        public WrongBirthDateException()
+        {
+        }
+
+        public WrongBirthDateException(string message)
+            : base(message)
+        {
+        }
+
+        public WrongBirthDateException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
